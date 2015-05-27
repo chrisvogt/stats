068b9fae8fa0e5f3e5218eebbfa049c91a705dce
Chris Vogt <mail@chrisvogt.me>
2015-05-26 22:07:59 -0700

    Clear out project readme and configure site name in boostrap file.

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -23,6 +23,7 @@
  * @since         CakePHP(tm) v 0.10.8.2117
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
+Configure::write('Site.title', 'Stats for CHRISVOGT.me');
 
 App::build(
 	[
